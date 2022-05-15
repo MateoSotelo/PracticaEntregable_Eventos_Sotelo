6aa2c659dff797d766a70a90d0d18f81f3787652
Msotelo1 <msotelo111201@gmail.com>
2022-05-15 14:56:17 -0300

    .

---
+++ PresentacionDepositos_PE/EntidadesDepositos_PE/ExtensionesRAM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesDepositos_PE
+{
+    public static class ExtensionesRAM
+    {
+        public static tipoMemoriaRAM ValidarRAM(this byte RAM)
+        {
+            switch (RAM)
+            {
+                case 2:
+                    return tipoMemoriaRAM.DosGb;
+                case 4:
+                    return tipoMemoriaRAM.CuatroGb;
+                case 8:
+                    return tipoMemoriaRAM.OchoGb;
+                case 16:
+                    return tipoMemoriaRAM.DieciseisGb;
+            }
+
+            return tipoMemoriaRAM.Invalido;
+        }
+    }
+}
